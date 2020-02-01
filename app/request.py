15854c2591d8d89841f12a0300cc15f9730d1b10
ian mwangi <iankimotho233@gmail.com>
2020-02-01 16:04:33 +0300

    Define a function to obtain news sources

--- app/request.py
+++ app/request.py
@@ -11,3 +11,21 @@ news_source_base_url = app.config['NEWS_API_SOURCE_BASE_URL']
 
 # Getting the News Source base url and configuring it to app.config
 news_article_base_url = app.config['NEW_API_ARTICLES_BASE_URL']
+
+
+def obtain_news_sources(category):
+    """Obtains the json response to the news sources url"""
+
+    news_sources_url = news_source_base_url.format(category, api_key)
+
+    with urllib.request.urlopen(news_sources_url) as source_url:
+        get_news_source_data = source_url.read()
+        get_news_source_response = json.loads(get_news_source_data)
+
+        news_sources_result = None
+
+        if get_news_source_response['sources']:
+            news_sources_result_list = get_news_source_response['sources']
+            news_sources_result = process_sources(news_sources_result_list)
+
+    return news_sources_result
