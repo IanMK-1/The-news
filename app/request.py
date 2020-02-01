a6d632e016ad1361e806965b54abc08e96fb5022
ian mwangi <iankimotho233@gmail.com>
2020-02-01 17:19:31 +0300

    Create a macro.html file that displays news sources list

--- app/request.py
+++ app/request.py
@@ -13,10 +13,10 @@ news_source_base_url = app.config['NEWS_API_SOURCE_BASE_URL']
 news_article_base_url = app.config['NEWS_API_ARTICLES_BASE_URL']
 
 
-def obtain_news_sources(category):
+def obtain_news_sources():
     """Obtains the json response to the news sources url"""
 
-    news_sources_url = news_source_base_url.format(category, api_key)
+    news_sources_url = news_source_base_url.format(api_key)
 
     with urllib.request.urlopen(news_sources_url) as source_url:
         get_news_source_data = source_url.read()
--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
+from .request import obtain_news_sources
 
 
 # views
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """Function that returns index page with its data"""
+
+    # Getting the news sources
+    all_news_sources = obtain_news_sources()
+    # print(all_news_sources)
+    return render_template('index.html', news_sources=all_news_sources)
