9137188706b4480e6fd8d30e39b04822821d5d79
ian mwangi <iankimotho233@gmail.com>
2020-02-02 09:41:31 +0300

    Create a models file to contian the model classes

--- app/models.py
+++ app/models.py
@@ -7,3 +7,14 @@ class NewsArticle:
         self.description = description
         self.publishedAt = publishedAt
         self.url = url
+
+
+class NewsSource:
+    """Class NewsSource creates instances of news sources"""
+
+    def __init__(self, id, name, country, description, url):
+        self.id = id
+        self.name = name
+        self.country = country
+        self.description = description
+        self.url = url
--- app/models/news_source.py
+++
@@ -1,9 +0,0 @@
-class NewsSource:
-    """Class NewsSource creates instances of news sources"""
-
-    def __init__(self, id, name, country, description, url):
-        self.id = id
-        self.name = name
-        self.country = country
-        self.description = description
-        self.url = url
